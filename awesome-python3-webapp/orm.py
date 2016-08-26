f0bbdc2f65bbab4dea088505c86b8dadaee519dc
wnjinly <wnjinly@163.com>
2016-08-26 11:34:18 +0800

    Model finished

--- awesome-python3-webapp/orm.py
+++ awesome-python3-webapp/orm.py
@@ -57,9 +57,9 @@ async def execute(sql,args,autocommit=True):
 			raise
 		return affected
 
-def create_arg_str(num):
+def create_args_string(num):
 	L = []
-	for n in rang(num):
+	for n in range(num):
 		L.append('?')
 	return ','.join(L)
 
@@ -74,7 +74,7 @@ class Field(object):
 	def __str__(self):
 		return '<%s, %s%s>'%(self.__class__.__name__,self.column_type,self.name)
 
-class StringFeild(Field):
+class StringField(Field):
 	def __init__(self,name=None,primary_key=False,default=None,ddl='varchar(100)'):
 		super().__init__(name,ddl,primary_key,default)
 
@@ -82,18 +82,56 @@ class IntegerField(Field):
 	def __init__(self,name=None,primary_key=False,default=0):
 		super().__init__(name,'bigint',primary_key,default)
 
-class BooleanFeild(Field):
+class BooleanField(Field):
 	def __init__(self,name=None,default=False):
 		super().__init__(name,'boolean',False,default)
 
-class FloatFeild(Field):
-	def __init__(self,naem=None,primary_key=False,default=0.0):
+class FloatField(Field):
+	def __init__(self,name=None,primary_key=False,default=0.0):
 		super().__init__(name,'real',primary_key,default)
 
-class TextFeild(Field):
+class TextField(Field):
 	def __init__(self,name=None,default=None):
 		super().__init__(name,'text',False,default)
 
+class ModelMetaclass(type):
+	def __new__(cls,name,bases,attrs):
+		#排除自己
+		if name == 'Model':
+			return type.__new__(cls,name,bases,attrs)
+		#获取table名称	
+		tableName = attrs.get('__table__',None) or name
+		logging.info('found model:%s(table:%s)'%(name,tableName))
+		#获取所有的Feild和主键名
+		mappings = dict()
+		fields = []
+		primaryKey = None
+		for k,v in attrs.items():
+			if isinstance(v,Field):
+				logging.info('found mapping:%s ==> %s'%(k,v))
+				mappings[k] = v
+				if v.primary_key:
+					#找到主键
+					if primaryKey:
+						raise RuntimeError('Duplicate primary key for field:%s'%k)
+					primaryKey = k
+				else:
+					fields.append(k)
+		if not primaryKey:
+			raise RuntimeError('Primary key not found.')
+		for k in mappings.keys():
+			attrs.pop(k)
+		escaped_fields = list(map(lambda f:'`%s`'%f,fields))
+		attrs['__mappings__'] = mappings #保存属性和列的映射关系
+		attrs['__table__'] = tableName
+		attrs['__primary_key__'] = primaryKey #主键名属性
+		attrs['__fields__'] = fields #除主键名外所有属性名
+		#构造默认的SELECT,INSERT,UPDATE和DELETE语句：
+		attrs['__select__'] = 'select `%s`,%s from `%s`' % (primaryKey,','.join(escaped_fields),tableName)
+		attrs['__insert__'] = 'insert into `%s` (%s,`%s`) values (%s)' % (tableName,','.join(escaped_fields),primaryKey,create_args_string(len(escaped_fields)+1))
+		attrs['__update__'] = 'update `%s` set %s where `%s`=?'% (tableName,','.join(map(lambda f: '`%s`=?'%(mappings.get(f).name or f),fields)),primaryKey)
+		attrs['__delete__'] = 'delete from `%s` where `%s`=?'%(tableName,primaryKey)
+		return type.__new__(cls,name,bases,attrs)
 
 
 
@@ -195,44 +233,6 @@ class Model(dict,metaclass=ModelMetaclass):
 
 
 
-class ModelMetaclass(type):
-	def __new__(cls,name,bases,attrs):
-		#排除自己
-		if name == 'Model':
-			return type.__new__(cls,name,bases,attrs)
-		#获取table名称	
-		tableName = attrs.get('__table__',None) or name
-		logging.info('found model:%s(table:%s)'%(name,tableName))
-		#获取所有的Feild和主键名
-		mappings = dict()
-		fields = []
-		primaryKey = None
-		for k,v in attrs.items():
-			if isinstance(v,Field):
-				logging.info('found mapping:%s ==> %s'%(k,v))
-				mappings[k] = v
-				if v.primary_key:
-					#找到主键
-					if primaryKey:
-						raise RuntimeError('Duplicate primary key for field:%s'%k)
-					primaryKey = k
-				else:
-					fields.append(k)
-		if not primaryKey:
-			raise RuntimeError('Primary key not found.')
-		for k in mappings.keys():
-			attrs.pop(k)
-		escaped_fields = list(map(lambda f:'`%s`'%f,fields))
-		attrs['__mappings__'] = mappings #保存属性和列的映射关系
-		attrs['__table__'] = tableName
-		attrs['__primary_key__'] = primaryKey #主键名属性
-		attrs['__fields__'] = fields #除主键名外所有属性名
-		#构造默认的SELECT,INSERT,UPDATE和DELETE语句：
-		attrs['__select__'] = 'select `%s`,%s from `%s`' % (primaryKey,','.join(escaped_fields),tableName)
-		attrs['__insert__'] = 'insert into `%s` (%s,`%s`) values (%s)' % (tableName,','.join(escaped_fields),primaryKey,create_args_string(len(escaped_fields)+1))
-		attrs['__update__'] = 'update `%s` set %s where `%s`=?'% (tableName,','.join(map(lambda f: '`%s`=?'%(mappings.get(f).name or f),fields)),primaryKey)
-		attrs['__delete__'] = 'delete from `%s` where `%s`=?'%(tableName,primaryKey)
-		return type.__new__(cls,name,bases,attrs)
 
 
 
