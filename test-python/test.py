11f37428fe5871436e2e1c8b0b2cacff395838ca
wnjinly <wnjinly@163.com>
2017-05-12 16:09:41 +0800

    添加test-python

---
+++ test-python/test.py
@@ -0,0 +1,27 @@
+from calculator import Count
+import unittest
+
+
+class TestCount(unittest.TestCase):
+
+    def setUp(self):
+        print("test start")
+
+    def test_add(self):
+        j = Count(2, 3)
+        self.assertEqual(j.add(), 5)
+
+    def test_add2(self):
+        j = Count(41, 76)
+        self.assertEqual(j.add(), 117)
+
+    def tearDown(self):
+        print("test end")
+
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    suite.addTest(TestCount("test_add2"))
+
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
